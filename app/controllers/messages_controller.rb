a694373cbb2760bc8f6686cda0f64b42b5322222
miokawai <miokawai1210@gmail.com>
2015-09-19 06:51:47 +0000

    first commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name , :body)
   end
   
 end
