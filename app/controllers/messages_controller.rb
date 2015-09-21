32dd6290cf37931722408fe224f0cbc90b075774
miokawai <miokawai1210@gmail.com>
2015-09-21 00:24:59 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,20 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
-     @messages = Message.all
-     @message = Message.new
+    @messages = Message.all
+    @message = Message.new
   end
   
   def create
     @message = Message.new(message_params)
-  if @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
-  else
-    # メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
-  end
+    if @message.save
+       redirect_to root_path, notice: 'メッセージを保存しました'
+    else
+       @messages = Message.all
+       flash.now[:alert] = "メッセージの保存に失敗しました。"
+       render 'index'
+    end
   end
  
   def edit
@@ -23,22 +22,24 @@ class MessagesController < ApplicationController
   
   def update
     if @message.update(message_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+    redirect_to root_path, notice: 'メッセージを編集しました'
     else
-      # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
   end
 
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+ 
   private
-  def message_params
-    params.require(:message).permit(:name , :body)
-  end
   
-  def set_message
-    @message = Message.find(params[:id])
-  end
-end
-  
+ def message_params
+   params.require(:message).permit(:name , :body)
+ end
 
+ def set_message
+  @message = Message.find(params[:id])
+ end
+end
